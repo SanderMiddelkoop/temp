83686cf910fa202f0042771df4a92f2c84884a8e
SanderMiddelkoop <42543146+SanderMiddelkoop@users.noreply.github.com>
2018-09-18 14:26:24 +0200

    Update IKlantService.java

--- com/example/demo/service/IKlantService.java
+++ com/example/demo/service/IKlantService.java
@@ -26,3 +26,13 @@ public interface IKlantService {
 	public List<Klant> findByAdress(String adress); 
 
 }
+
+# H2
+#spring.h2.console.enabled=true
+#spring.h2.console.path=/h2
+
+# Datasource
+#spring.datasource.url=jdbc:h2:mem:test
+#spring.datasource.username=sa
+#spring.datasource.password=
+#spring.datasource.driver-class-name=org.h2.Driver
